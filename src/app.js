0d08afb54e569838f1077c55df404bf8d478a119
luciacanalda <luciacanaldas@gmail.com>
2024-10-21 18:27:27 +0000

    Cambios

--- src/app.js
+++ src/app.js
@@ -6,6 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#generate-excuse").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
 };
+
+// Excuse Generator
+
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let WhoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  document.querySelector("#the-excuse").innerHTML;
+
+  return `${who[WhoIndex]}${action[actionIndex]}${what[whatIndex]}${when[whenIndex]}`;
+}
+
+// funciones por expresión
+
+//funciones anónimas o funciones flechas o arrow functions
